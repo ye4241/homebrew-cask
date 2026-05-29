e171c7269cce0a1e62a4762d6b6fbf228c288b98
ye4241 <6803102+ye4241@users.noreply.github.com>
2026-05-29 14:17:29 +0800

    dotnet-reactor: use signed and notarized build

--- Casks/d/dotnet-reactor.rb
+++ Casks/d/dotnet-reactor.rb
@@ -1,8 +1,8 @@
 cask "dotnet-reactor" do
   version "7.5.0.0"
-  sha256 "12ce2e1a3fa761a24b21982a15a470947389081d48feb72e684f45e3523b13b0"
+  sha256 "cb3ec024fd7ae2690c72b538b5f6810288825205636e03db520dead0b0fbb2c2"
 
-  url "https://cdn-downloads.eziriz.com/dotnet_reactor_#{version.dots_to_underscores}_macos-x64.zip"
+  url "https://www.eziriz.com/downloads/dotnet_reactor_#{version.dots_to_underscores}_macos-x64_signed.zip"
   name ".NET Reactor"
   desc ".NET code protection and obfuscation tool"
   homepage "https://www.eziriz.com/dotnet_reactor.htm"
@@ -14,9 +14,4 @@ cask "dotnet-reactor" do
 
   binary "dotNET_Reactor"
   binary "dotNET_Reactor.Console"
-
-  postflight do
-    system_command "/usr/bin/xattr",
-                   args: ["-dr", "com.apple.quarantine", staged_path]
-  end
 end
